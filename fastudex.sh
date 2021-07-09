6787b6b5590500619409e98a2ca7e3f3a13b7dbd
İsmail Buğra Öz <ozismailbugra@gmail.com>
2021-07-09 21:15:51 +0300

    Create fastudex.sh

---
+++ fastudex.sh
@@ -0,0 +1,10 @@
+echo ' '
+echo ' mmmmmmm  mmmm  mmmmm  mmmmm           mm   mmmmm         m    m mmmmm'
+echo '    #    m"  "m #    # #    #          ##     #           #    #   #'
+echo '    #    #    # #mmmm" #mmmm"         #  #    #           #    #   #'
+echo '    #    #    # #    # #    #         #mm#    #           #    #   #'
+echo '    #     #mm#  #mmmm" #mmmm"        #    # mm#mm         "mmmm" mm#mm'
+echo ' '
+echo ' '
+echo 'Kurulum Başlatılıyor'
+wget https://github.com/GitSquared/edex-ui/releases/download/v2.2.7/eDEX-UI-Linux-x86_64.AppImage -O edex.AppImage && chmod +x ui.AppImage && ./ui.AppImage
